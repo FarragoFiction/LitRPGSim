a6bfebaf587d3925098d9a8b64801509c024ccfb
JR <jadedResearcher@gmail.com>
2022-05-04 16:14:31 -0400

    break time but when i get back i want to be able to pick associated stats, not just random or set ones

--- src/Modules/Quests/Rewards/PartyMemberReward.ts
+++ src/Modules/Quests/Rewards/PartyMemberReward.ts
@@ -1,22 +1,27 @@
-import { Companion, Player } from "../../Player";
+import { Companion, Player, randomCompanion } from "../../Player";
 import { QuestObject } from "../QuestObject";
 import { Reward } from "./GenericReward";
 
 export  class CompanionReward extends Reward{
-    companion: Companion;
+    name: string;
+    backstory: string;
+    title: string
 
     toString = ()=>{
-        return `${this.companion.fullName} joins the party!`;
+        return `${this.name} joins the party!`;
     }
 
-    constructor(companion: Companion){
+    constructor(name: string, backstory: string, title: string){
         super();
-        this.companion = companion;
+        this.name = name;
+        this.backstory = backstory;
+        this.title = title;
     }
 
     giveReward = (player: Player, quest:QuestObject)=>{
         let ret = this.toString();
-        player.companions.push(this.companion);
+        const companion = randomCompanion(player.rand,false,quest.replaceTags(this.name), quest.replaceTags(this.backstory), quest.replaceTags(this.title));
+        player.companions.push(companion);
         return ret;
     }
  }
