e41b203312b799ea8eab3530f0c58d246f3d2f19
JR <jadedResearcher@gmail.com>
2022-05-26 19:11:16 -0400

    edits

--- src/Screens/Attic/TrueJR.ts
+++ src/Screens/Attic/TrueJR.ts
@@ -38,14 +38,14 @@ export const Two = ()=>{
 
     const ramble = new JRRamble("Yeah...", []);
 
-    ramble.potential_reponses.push(new PlayerResponse("You seem ...sadder than normal?", Three));
+    ramble.potential_reponses.push(new PlayerResponse("So this is where you talk to me directly?", Three));
 
     return ramble;
 }
 
 export const Three = ()=>{
 
-    const ramble = new JRRamble("Yeah. Don't get me wrong, though. Zampanio has been a lot of fun and I can't imagine stopping any time soon...", []);
+    const ramble = new JRRamble("Yeah. If you want to? Don't get me wrong, Zampanio has been a lot of fun and I really love that people are still lost in it...", []);
 
     ramble.potential_reponses.push(new PlayerResponse("It's just...sometimes I worry.", Four));
 
@@ -72,7 +72,7 @@ export const Five = ()=>{
 
 export const Six = ()=>{
 
-    const ramble = new JRRamble("I want to not be alone. ", []);
+    const ramble = new JRRamble("I really like it when there's people here. Seeing what I'm creating. Creating their own things and showing me. Documenting what they've found, so I can see. ", []);
 
     ramble.potential_reponses.push(new PlayerResponse("I want people to be here with me in this maze I have created, expanding it with me.", Seven));
 
@@ -81,25 +81,25 @@ export const Six = ()=>{
 
 export const Seven = ()=>{
 
-    const ramble = new JRRamble("I also want them to be safe, and to not be trapped.", []);
+    const ramble = new JRRamble("I also want them to be safe, and to not be get so obsessed they feel trapped.", []);
 
-    ramble.potential_reponses.push(new PlayerResponse("In Truth, the goals aren't actually conflicting.", Eight));
+    ramble.potential_reponses.push(new PlayerResponse("In Truth, the goals aren't conflicting.", Eight));
 
     return ramble;
 }
 
 export const Eight = ()=>{
 
-    const ramble = new JRRamble("The best way I can secure long term creative collaboration and feedback is to make sure those fallen down the rabbithole don't burn themselves out.", []);
+    const ramble = new JRRamble("Even tho sometimes it feels that way. ", []);
 
-    ramble.potential_reponses.push(new PlayerResponse("At work I've volunteered to spearhead the morale improvement measures-- actual real changes rather than just bandaids. Because we have so much attrition. ", Nine));
+    ramble.potential_reponses.push(new PlayerResponse("People should leave if they're not having fun. And at the same time, I can take steps to try to prevent it from going sour? ", Nine));
 
     return ramble;
 }
 
 export const Nine = ()=>{
 
-    const ramble = new JRRamble("And the way I feel is that... If something is bad, you gotta leave. It makes sense.  But I can still hope that I can fix the bad parts enough that at least SOME people stay?", []);
+    const ramble = new JRRamble("I can remind you to hydrate and takes breaks and all that. I can encourage community, in my own way, even if I'm still hiding, so that we all can work together to create thing.", []);
 
     ramble.potential_reponses.push(new PlayerResponse("It's one reason why I'm constantly reminding people to document their findings, update the wiki, make ao3 fics, whatever.  So that... This impossibly huge maze. Can feel just that bit smaller. When you remember you aren't alone in it. That other people are working along with you to map it out. ", Ten));
 
@@ -136,9 +136,9 @@ export const Twelve = ()=>{
 
 export const Thirteen = ()=>{
 
-    const ramble = new JRRamble("So what I can do is warn people. Wrap what I create in a haze of danger. Hide it so that only those already in the obession throes can find it.  Remind them that obsession is bad.  But...let myself enjoy creation. It can just be for me. It can be always with the knowledge that it is temporary. This too, shall pass.  One day, possibly soon, all the current Unmarked will have their stories leave Zampanio's and this entire thing will be nostalgic. I'll be excited if I find anyone transversing this maze, no matter which direction they pick, since it will be a throw back. History. One day I will be history and if nothing else, my Past Self has shown me how to navigate that. ", []);
+    const ramble = new JRRamble("So what I can do is warn people. Wrap what I create in a haze of danger. Hide it so that only those already in the obession throes can find it.  Remind them that obsession is bad.  But...let myself enjoy creation. It can just be for me. It can be always with the knowledge that it is temporary. This too, shall pass.  One day, inevitably, all the current Unmarked will have their stories leave Zampanio's and this entire thing will be nostalgic.  One day I will be history and if nothing else, my Past Self has shown me how to navigate that. ", []);
 
-    ramble.potential_reponses.push(new PlayerResponse("So yeah. No matter if you find this when ZampanioSim is actively still shifting like so many grains of sand. Or if you find it far in the future afer the rot has consumed it. I hope you're staying safe. I hope this is fun but not so fun you stop doing other things. I hope you create things that feed you and bring you joy but also take the time to consume things as well. Play games. Read books. Touch grass, I dunno. Just. Remember that Obsession Leads To Ruin.", Thirteen));
+    ramble.potential_reponses.push(new PlayerResponse("So yeah. No matter if you find this when ZampanioSim is actively still shifting like so many grains of sand. Or if you find it far in the future afer the rot has consumed it. I hope you're staying safe. I love you leave your Mark. I hope this is fun but not so fun you stop doing other things. I hope you create things that feed you and bring you joy but also take the time to consume things as well. Play games. Read books. Touch grass, I dunno. Just. Remember that Obsession Leads To Ruin.", Thirteen));
 
     return ramble;
 }
