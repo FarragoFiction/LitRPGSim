3fc2b00fd912b48103cbd7da6b04017c50f11a21
JR <jadedResearcher@gmail.com>
2022-06-08 22:54:13 -0400

    newspost, forgot how funny all the broken links are

--- src/About.tsx
+++ src/About.tsx
@@ -58,6 +58,9 @@ export const Post = styled.tr`
   padding: 10px;
   border: 1px solid black;
   background-color: #ffefc4;
+  a{
+    pointer-events: none;
+  }
 `
 
 export const Date = styled.span`
@@ -90,7 +93,8 @@ function NewspostDom(props: PostProps) {
 }
 function About(props: AppProps) {
   const newsposts = [
-    new Newspost("04/23/2022", "I made a <a href ='http://farragofiction.com/BlorboBio/?data=N4IgdghgtgpiBcIAqALGACAomAJiANCDjAM4CWA5pAC5kD2YCIAMgLQAMrAjAOwEgAnMiQDWTABKZ+1NLBJMAIgHklAWXxcuAZnyYAcgoCSegOIBlDdvwBpPYZPik+ABzt8AJUwAFT2bOGlPUsdZkDMZgBNfAAmEMwAQQVMd3wAVlT8MwBhcSVmeJTY-hJqCGp5REwADSRkvXjmfABOHS94pEN9J00dQySGwzMOrOadLIBVdwDxi3T8UIiG0d09ZJNMYfxWFvwTPSV-C2j+GAAPCABjagAbAE8AfRkBGBhHsgAHCpAAdTQwdAg6BIAFcAEYAKxgV3Q1DowIuaBIMLQ6AudAAZuiyP9kTBbgDnuh0XDcOgAO5kGS4sgCdBoCA4IHXMgXGCMi7XGAQf4Y9EAOnwhnR6DYnF46BgYAoEAopHQ2PQ8WopQRsDA1HJdAEIlx+IghOJwNJFKpMhgNLpXMZJGZrPZnO56F5AtF3B48qROAEEDJ-1hEuZUGxZWxFFRMAEpQVpQEsvKAI1jogADcIzKMN7qBgMU6wBh3hH0FAGDI+fxsSVsVcmFk6FBi2B8LX6wwm3WG22W43mw3y2ByBQUNQa+3Wz2x6Pu5PO73CGUVSg1cPEAA1CP48SUFD4TeDmet5h0Mn4NcCfGHsn8Z7vZ4kcgMJgX+ZH-dTrs7rfFUqgsjM6gPTlU2uJhtCvNl7mIdFJRIOBEC4PljkIMkUEpV5IOg2CQHYPlnH4UFrkuEQIJgKD+0w7DUn4d4IE5YjSJgkC+UowgyCgCh7hIAQLiYIdqHeeAAHoBPRfVvQoDEWVoBg+TRKABIALWgaiwHocQ6BubERAEs17klHBoj5d4pS-LUHmIKNri+d4CNZFA6GuYgBBAABfIA'>blorbo maker</a>. ","Though the downside is that if you try to click any link sin this newspost it absolutely loses its shit.")
+     new Newspost("06/08/2022","lol i keep forgetting to update this. i added a buncha quests, changed the vibes of the game and also the stuff you can find in the various basements.  and the attic? i think? probably the attic. BlorboBio has been replaced, btw. Don't worry about it.","Don't forget to <a href = 'http://knucklessux.com/HydrationSim/?seed=8270279'>Hydrate</a> :) :) :)")
+    ,new Newspost("04/23/2022", "I made a <a href ='http://farragofiction.com/BlorboBio/?data=N4IgdghgtgpiBcIAqALGACAomAJiANCDjAM4CWA5pAC5kD2YCIAMgLQAMrAjAOwEgAnMiQDWTABKZ+1NLBJMAIgHklAWXxcuAZnyYAcgoCSegOIBlDdvwBpPYZPik+ABzt8AJUwAFT2bOGlPUsdZkDMZgBNfAAmEMwAQQVMd3wAVlT8MwBhcSVmeJTY-hJqCGp5REwADSRkvXjmfABOHS94pEN9J00dQySGwzMOrOadLIBVdwDxi3T8UIiG0d09ZJNMYfxWFvwTPSV-C2j+GAAPCABjagAbAE8AfRkBGBhHsgAHCpAAdTQwdAg6BIAFcAEYAKxgV3Q1DowIuaBIMLQ6AudAAZuiyP9kTBbgDnuh0XDcOgAO5kGS4sgCdBoCA4IHXMgXGCMi7XGAQf4Y9EAOnwhnR6DYnF46BgYAoEAopHQ2PQ8WopQRsDA1HJdAEIlx+IghOJwNJFKpMhgNLpXMZJGZrPZnO56F5AtF3B48qROAEEDJ-1hEuZUGxZWxFFRMAEpQVpQEsvKAI1jogADcIzKMN7qBgMU6wBh3hH0FAGDI+fxsSVsVcmFk6FBi2B8LX6wwm3WG22W43mw3y2ByBQUNQa+3Wz2x6Pu5PO73CGUVSg1cPEAA1CP48SUFD4TeDmet5h0Mn4NcCfGHsn8Z7vZ4kcgMJgX+ZH-dTrs7rfFUqgsjM6gPTlU2uJhtCvNl7mIdFJRIOBEC4PljkIMkUEpV5IOg2CQHYPlnH4UFrkuEQIJgKD+0w7DUn4d4IE5YjSJgkC+UowgyCgCh7hIAQLiYIdqHeeAAHoBPRfVvQoDEWVoBg+TRKABIALWgaiwHocQ6BubERAEs17klHBoj5d4pS-LUHmIKNri+d4CNZFA6GuYgBBAABfIA'>blorbo maker</a>. ","Though the downside is that if you try to click any link sin this newspost it absolutely loses its shit.")
 
     ,new Newspost("04/22/2022", "Good news: the muzak i found probably has hypnotic qualities.  Also good news, i can't stop listening it.","Ria warned me. <a href = 'http://farragofiction.com/BlorboBio/?data=N4IgdghgtgpiBcICyBVAWgQQNIgDQgBMYBnASwHNIAXUgezARAGUBaAYRYCUBWPEAJ1LEA1owwAZAKIAFABJ8qACxixiY6QHk2EgJrSmk3AAIAjCYDMxpJOsAVAJJtjZy0dRNHzi8YBinDGy2EvCm3kbSKNLSkrYh5uZ8xFQQVGqI9gBytpKcGRK4AJyW0hgOklm4Lrj2ACKSEvZMDk4muAFsMg4ZAOKV3uIaOvlV2uJIfQn4MAAeEADGVAA2AJ4A+kr8MDDrpAAOaSAYYMtGTACuAEYAVjALRrQAZkbmRvZgZOSKVPf8RuK0AHcYL8hEZSGA5hBdhALosYPcnnN6MQ5nQzsQVkZdsC5jBSAA3cHkU7sLjcADkxCMxHo5AAdMZ7E8lDAThBNkYZrtaMQYAQjFRaCSODwjA9aL8oBL4UoIGAjNwjEj3rczjR8fCoOC1SQBcoTgDSItFkYLvCkVBsca+QKhcRdpsIPzWCLFYL7izfuyYBBiAyjBgqYtaUZfcKyWCqbtFhBlkSwd9Dcaxfx5sljScqIaknr4cHITR6AnI2D5bQNV75Vz6DAwDQIBnS3NHWQwMTUwRSOi6XxwUlwQtGAA1YEnf4A3Aj-hjwGT0d-WdTmcTpcLgG994UL7D+fjufTtf75dHw+yLd8FLJOaKWB1ncHverx+7xcvldvvibB0kMj0e-Hp9XwfIDjzPT5EmSC4jVIKg1jhDVFkYAAGOkkM-PlViIB5a15ZDUL4AFFBg7YsJwuBEBQ3h8FheZhEwmBsJVPC0PwaE4XoxjcMQAAmfD8FIKByFWYh+DmRgvioXZ4AAemkh52VTchHlIBY6DAOkLWktBoGhMA6FkWglnBYRpIdWhoXIOUCAgOldjbCCJTWIhkiNA5o3mGBFFoRYiH4EAAF8gA'>  Tho she still listens to it too.</a>")
 
@@ -117,6 +121,7 @@ function About(props: AppProps) {
             <li>Violent, Creepy Monsters Described in Text</li>
             <li>Player Blaming</li>
             <li>Player Insulting</li>
+            <li>Rabbitholes of Obession</li>
             <li>Addiction Themes</li>
             <li>Strong Langauge (Sorry, that's all me, Zampanio itself is probably not so sweary)</li>
           </ul>
@@ -128,87 +133,8 @@ function About(props: AppProps) {
         </div>
       </AboutDom>
 
-      <Gigglesnort>
-        <h2>Helpful Hints For Secrets :) :) :)</h2>
-        <p>So, it's come to my attention that secrets spiralling forever in all directions is not a particularly CHILL thing to experience???
-        </p>
-        <p>Apparently I got the wrong lesson from Easter Fools that one year?</p>
-
-        <p>So, time for me to bust out the traditional gigglesnort hidey talk to list out every secret I remember there being (in the simulation ONLY not any of the paths out of it) in such a way that HOPEFULLY is barely helpful (or even actively harmful) in terms of finding anything BUT once you've found it you easily recognize it in the list and can check it off!!!
-        </p>
-        <p>If you've completed your checklist (or even if you're just particularly proud of your progress), let me know! I'm either jadedResearcher or justifiedRecursion on gmail, and if you aren't afraid of the spotlight feel free to edit the wiki (add a page for yourself?) listing what you <b>think</b> the answers to all of these are. I particularly enjoy how much like lies most of the answers sound like :) :) :)
-        </p>
-        <p>I am ABSOLUTELY pro spoilers for the wiki so long as you've marked them, since AFTER all we need to let our more puzzle challenged associates have a chance to become obsessed with Zampanio as well.  I would appreciate it a lot if people updated the wiki with everything they think is true and everything they want to be true.
-        </p>
-        <p>If you get my attention in a timely fashion, I may do a shout out to you! Or work you in in some fashion.</p>
-
-        <p>Demonstrating obsession with Zampanio won't HURT your chances of becoming Marked, that's for sure.</p>
-        <p>This experience will absolutely be changing over time, both in response to the things I see people doing and just because I doubt I'll ever truly break free. Decide whether you prefer to engage early, when you'll still stand out, or late, when you'll blend into the crowd.  Or after its all over, when you can piece together the entire thing at your leisure with no worries about being seen.</p>
-
-        <p>Bonus points if you show your work through a spiralling fanwork of your own.  I particularly enjoy logs of investigations and rambling disjoint streams of consciousness. </p>
-
-        <p>Feed me.</p>
-        <h3>MainPath: </h3>
-        <ul>
-          <li> Do you know how to walk, jump and skip cutscenes?</li>
-          <li>Do you understand why the menu cannot be closed?</li>
-          <li>Have you found what lurks behind the menu?</li>
-          <li>Have you found what walking reveals?</li>
-          <li>How many versions of the background music are there?</li>
-          <li>How do you reach the rabbit hole? What prevents you?</li>
-          <li>Where would you find passwords? Bonus points if you know the inspiration for each. </li>
-          <li>Can you trust words?</li>
-          <li>Can you trust your eyes?</li>
-          <li>What if those eyes are finally real?</li>
-          <li>Do you recognize what you hear when real eyes are touched?</li>
-          <li>Is it worth it, to make that which is not a spiral become locked into a spiral?</li>
-          <li>Is it worth it, to make that which is not a game become a game?</li>
-          <li>Where is it too dangerous to create a waste land of Truth?</li>
-          <li>Where can you find JR's attic? </li>
-        </ul>
-
-        <h3>Truth: </h3>
-        <ul>
-          <li>Is it worth it to translate binary?</li>
-          <li>How do you reach JustTruth?</li>
-          <li>What is Truth's desire?</li>
-          <li>Where can you find JR's attic?</li>
-        </ul>
-
-        <h3>ThisIsAGame</h3>
-        <ul>
-          <li>Is it worth it to use your skills?</li>
-          <li>How do you use your inventory?</li>
-          <li>Is it safe to  talk to your friends?</li>
-          <li>How do you unlock a door?</li>
-          <li>Is it worth it to kill?</li>
-          <li>How do you meet NotAMinotaur?</li>
-          <li>What does NotAMinotaur tell you?</li>
-          <li>How do you meet the ShamblingHorrorWithYourFace?</li>
-          <li>Does it hurt to die?</li>
-          <li>What happens when you collect 9 artifacts?</li>
-          <li>What happens if you ignore NotAMinotaur's pleas?</li>
-          <li>Is ending the world worth it?</li>
-          <li>Where can you find JR's attic?</li>
-        </ul>
-        <h3>PathsOut</h3>
-
-        <p>These are secrets that lead AWAY from this simulation. The end is never the end, after all!!!</p>
-        <p>Some of them are dead ends. Some are red herrings. Some lead infinitely outwards. </p>
-        <p>Feel free to use them yourselves. A dead end, afterall, doesn't need to STAY a deadend if you're willing to forge your own path.</p>
-        <ul>
-          <li>Where can you peel back all illusions?</li>
-          <li>Where can you watch those who came before?</li>
-          <li>Where can you spiral endlessly in maps and stories and telling what you saw and correcting lies and adding lies and giving hints and taking hints and confusing everyone forever?</li>
-          <li>Where can you put a cassette tape?</li>
-          <li>Where do you find a PuzzleBox?</li>
-          <li>Where can you become lost in fragmented, echoing, reflected thoughts?</li>
-          <li>Where can you go to see a shit post?</li>
-
-        </ul>
 
 
-      </Gigglesnort>
 
       <DevLog>
 
